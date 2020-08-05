a97e4869336ba2baf48186c6501c7db79d829f31
thiagotp <thiagoteixeira.ps@gmail.com>
2020-08-05 13:15:32 -0300

    Editando a exibição da data

--- Calculadora em JS/scripts/controller/calcController.js
+++ Calculadora em JS/scripts/controller/calcController.js
@@ -60,7 +60,11 @@ class CalcController {
     //MÉTODOS DA APLICAÇÃO
 
     setDisplayDateTime() {
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric'
+        });
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 }
