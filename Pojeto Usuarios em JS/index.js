bef8eb4fa20ab82dc8bbd4b4dbafda74c6e7da96
thiagotp <thiagoteixeira.ps@gmail.com>
2020-08-22 21:02:51 -0300

    implementando conceitos básicos de manipulação do DOM e JSON

---
+++ Pojeto Usuarios em JS/index.js
@@ -0,0 +1,26 @@
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+
+
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
+
+
+fields.forEach((field, index) => {
+    if(field.name === "gender"){
+        if(field.checked){
+            user[field.name] = field.value;
+        }
+    }else{
+        user[field.name] = field.value;
+    }
+});
+
+console.log(user)
